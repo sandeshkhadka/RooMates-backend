20317833993ff6e03ce0540297af7596200e14f2
sandeshkhadka <sandeshkhadka217@gmail.com>
2023-08-11 16:36:38 +0545

    add missing signin/up middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import router from "./router";
-import { authMiddleware } from "./modules/middlewares";
+import {
+  authMiddleware,
+  signInMiddleware,
+  signUpMiddleware,
+} from "./modules/middlewares";
 import { signin, signup } from "./handlers/signin_signup";
 import { body } from "express-validator";
 import morgan from "morgan";
@@ -11,8 +15,18 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/signup", body(["username", "password", "email"]).exists(), signup);
-app.post("/signin", body(["username", "password"]).exists(), signin);
+app.post(
+  "/signup",
+  body(["username", "password", "email"]).exists(),
+  signUpMiddleware,
+  signup,
+);
+app.post(
+  "/signin",
+  body(["username", "password"]).exists(),
+  signInMiddleware,
+  signin,
+);
 
 app.use("/api", authMiddleware, router);
 export default app;
